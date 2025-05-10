a651239459a65393bdadb48e1b905eb61eac70d7
PedroBarros3421 <ppedro3421@gmail.com>
2025-05-10 15:57:51 -0300

    Add Terraform configuration files and variables for AWS EC2 setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,76 @@
 # Terraform configuration file
+
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "awspessoal"
+  region  = "us-east-1"
+}
+
+# Busca a AMI mais recente do Amazon Linux 2 em us-east-1
+data "aws_ami" "amazon_linux2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_security_group" "app_sg" {
+  name_prefix = "app-sg-"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Permite HTTP de qualquer lugar
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["177.37.13.10/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Permite todo o tráfego de saída
+  }
+
+  tags = {
+    Name = "EC2 DevOps Pos Graduacao"
+  }
+}
+
+resource "aws_instance" "app" {
+  ami           = data.aws_ami.amazon_linux2.id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.app_sg.id] # Associa o SG
+
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name = "EC2 DevOps Pos Graduacao"
+    Team = "4tetofantastico"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "key_name" {
+  description = "Nome do key pair já criado na AWS (usado para SSH)"
+  type        = string
+  default     = "meu-key-pessoal"
+}
+
+variable "instance_type" {
+  description = "Tipo da instância EC2"
+  type        = string
+  default     = "t2.micro"
+}
