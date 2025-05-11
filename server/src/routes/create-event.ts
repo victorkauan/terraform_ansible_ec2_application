8212ba1968f1f731945e9ca60b4423e2123cee4d
Mary Santos <marysantoslima24@gmail.com>
2025-05-10 21:03:17 -0300

    Criação de rotas e tabelas
    Criado as rotas para os end points de get e post dos participantes, eventos e registro
    Criado a tabela de Participantes

---
+++ server/src/routes/create-event.ts
@@ -0,0 +1,51 @@
+import z from "zod";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { generateSlug } from "../utils/generate-slug";
+import { prisma } from "../lib/prisma";
+import { FastifyInstance } from "fastify";
+
+export async function createEvent(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/events",
+    {
+      schema: {
+        body: z.object({
+          title: z.string().min(4),
+          details: z.string().nullable(),
+          maximunAttendees: z.number().int().positive().nullable(),
+        }),
+        response: {
+          201: z.object({
+            eventId: z.string().uuid(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const data = request.body;
+
+      const slug = generateSlug(data.title);
+
+      const eventWithSameSlug = await prisma.event.findUnique({
+        where: {
+          slug,
+        },
+      });
+
+      if (eventWithSameSlug !== null) {
+        throw new Error("Já existe outro evento com esse título");
+      }
+
+      const event = await prisma.event.create({
+        data: {
+          title: data.title,
+          details: data.details,
+          maximunAttendees: data.maximunAttendees,
+          slug,
+        },
+      });
+
+      return reply.status(201).send({ eventId: event.id });
+    }
+  );
+}
