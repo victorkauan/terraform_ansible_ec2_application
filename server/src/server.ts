2a13148dffcb25ea27a99314de95f689b7b2efd4
Mary Santos <marysantoslima24@gmail.com>
2025-05-24 13:36:32 -0300

    Freature: Configurando o Env
    Configurações do .env e criação do .env.example para informar o exemplo de .env que deve ser criado

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import fastify from "fastify";
+import "dotenv/config";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 import {
@@ -14,6 +15,8 @@ import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { getEventAttendees } from "./routes/get-event-attendees";
 
 const app = fastify();
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.register(fastifySwagger, {
   swagger: {
@@ -41,6 +44,11 @@ app.register(getEvent);
 app.register(getAttendeeBadge);
 app.register(getEventAttendees);
 
-app.listen({ port: 2424, host: "0.0.0.0" }).then(() => {
-  console.log("Server is running on http://localhost:2424/docs");
+app.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server is running at ${address}`);
+  console.log(`Swagger docs available at ${address}/docs`);
 });
