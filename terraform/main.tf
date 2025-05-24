7a1c44f9e758a3dfe08182c11c2f885e78376f9f
PedroBarros3421 <ppedro3421@gmail.com>
2025-05-24 10:22:14 -0300

    Remove ingress rule for port 3000 and update EC2 public IP and DNS in state file

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,13 +27,6 @@ resource "aws_security_group" "app_sg" {
   }
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
