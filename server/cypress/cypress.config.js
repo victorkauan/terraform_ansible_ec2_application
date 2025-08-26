63adcda833fb8395a1c995a699f4d76e7a816fe1
Karine Costa <karine.costadev@gmail.com>
2025-08-26 10:59:56 -0300

    ajustanndo pipe

--- server/cypress/cypress.config.js
+++ server/cypress/cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000', // Ajuste para seu backend
-    supportFile: 'cypress/support/e2e.js',
-    specPattern: 'tests/api/**/*.cy.js',
+      supportFile: 'cypress/support/e2e.js',
+      specPattern: 'cypress/tests/api/**/*.cy.js',
     viewportWidth: 1280,
     viewportHeight: 720,
 
