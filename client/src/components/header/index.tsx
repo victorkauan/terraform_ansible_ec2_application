aab1c3eb2414ccd32c276e2df13fd5e2ae6ce2ba
Victor Kauan <victorkauan.dev@gmail.com>
2025-10-25 13:16:19 -0300

    Refactor header links using React Router NavLink component

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,43 +1,44 @@
+import { NavLink } from "react-router";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 
 export default function Header() {
   return (
     <header className="bg-white shadow px-6 py-4 sticky top-0">
       <nav className="max-w-7xl mx-auto flex items-center justify-between">
-        <a href="/" className="flex items-center gap-1.5">
+        <NavLink to="/" className="flex items-center gap-1.5">
           <CalendarTodayIcon />
           <h1 className="text-xl font-semibold">EventHub</h1>
-        </a>
+        </NavLink>
 
         <ul className="gap-6 hidden sm:flex">
           <li>
-            <a href="/" className="text-sm font-semibold">
+            <NavLink to="/" className="text-sm font-semibold">
               Eventos
-            </a>
+            </NavLink>
           </li>
           <li>
-            <a href="/events/create" className="text-sm font-semibold">
+            <NavLink to="/events/create" className="text-sm font-semibold">
               Criar evento
-            </a>
+            </NavLink>
           </li>
         </ul>
 
         <ul className="flex items-center gap-2">
           <li>
-            <a
-              href="/admin/dashboard"
+            <NavLink
+              to="/admin/dashboard"
               className="text-sm font-semibold text-center min-h-8 px-2.5 py-1.5 rounded-lg flex items-center justify-center hover:bg-neutral-100"
             >
               Admin
-            </a>
+            </NavLink>
           </li>
           <li>
-            <a
-              href="/events/create"
+            <NavLink
+              to="/events/create"
               className="text-white text-sm font-semibold text-center bg-black min-h-8 px-2.5 py-1.5 rounded-lg flex items-center justify-center hover:bg-neutral-800"
             >
               Criar evento
-            </a>
+            </NavLink>
           </li>
         </ul>
       </nav>
