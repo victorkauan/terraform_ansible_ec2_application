743c641e64b39aaa0ecb9b5326785f8696f750e6
Victor Kauan <victorkauan.dev@gmail.com>
2025-10-25 12:05:27 -0300

    Install and configure Axios

---
+++ client/src/api/index.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:2424";
+
+const api = axios.create({ baseURL: API_BASE_URL });
+
+export default api;
