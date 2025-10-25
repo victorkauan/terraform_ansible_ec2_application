66216a6a4d774948a0666bf27e30f1b9d319b842
Victor Kauan <victorkauan.dev@gmail.com>
2025-10-25 13:16:34 -0300

    Create new event page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import EventsPage from "./pages/events";
+import CreateEventPage from "./pages/create-event";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route index element={<EventsPage />} />
+        <Route path="events">
+          <Route path="create" element={<CreateEventPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
