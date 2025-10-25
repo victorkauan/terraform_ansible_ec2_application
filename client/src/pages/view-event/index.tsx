13185a0dc805fc85483ea18b8524595ab29e97a6
Victor Kauan <victorkauan.dev@gmail.com>
2025-10-25 15:48:56 -0300

    Use back button component

--- client/src/pages/view-event/index.tsx
+++ client/src/pages/view-event/index.tsx
@@ -1,8 +1,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Link } from "react-router";
-import WestIcon from "@mui/icons-material/West";
 import queryClient from "../../api/query-client";
 import Header from "../../components/header";
+import BackButton from "../../components/back-button";
 import ViewEvent from "../../components/view-event";
 
 export default function ViewEventPage() {
@@ -11,13 +10,7 @@ export default function ViewEventPage() {
       <section>
         <Header />
         <main className="max-w-4xl p-6 mx-auto">
-          <Link
-            to="/"
-            className="text-neutral-600 text-sm mb-6 flex items-center gap-2 transition-colors hover:text-black"
-          >
-            <WestIcon fontSize="inherit" />
-            <span>Voltar para eventos</span>
-          </Link>
+          <BackButton to="/" message="Voltar para eventos" />
           <ViewEvent />
         </main>
       </section>
