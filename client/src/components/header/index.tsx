54b4404aa2fc73d293cb8d19aac4b9434da38f20
Victor Kauan <victorkauan.dev@gmail.com>
2025-10-25 11:55:52 -0300

    Create header component

---
+++ client/src/components/header/index.tsx
@@ -0,0 +1,46 @@
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+
+export default function Header() {
+  return (
+    <header className="shadow px-6 py-4">
+      <nav className="max-w-7xl mx-auto flex items-center justify-between">
+        <a href="/" className="flex items-center gap-1.5">
+          <CalendarTodayIcon />
+          <h1 className="text-xl font-semibold">EventHub</h1>
+        </a>
+
+        <ul className="gap-6 hidden sm:flex">
+          <li>
+            <a href="/" className="text-sm font-semibold">
+              Eventos
+            </a>
+          </li>
+          <li>
+            <a href="/events/create" className="text-sm font-semibold">
+              Criar evento
+            </a>
+          </li>
+        </ul>
+
+        <ul className="flex gap-2">
+          <li>
+            <a
+              href="/admin/dashboard"
+              className="text-sm font-semibold h-8 px-2.5 rounded-lg flex items-center justify-center hover:bg-neutral-100"
+            >
+              Admin
+            </a>
+          </li>
+          <li>
+            <a
+              href="/events/create"
+              className="text-white text-sm font-semibold bg-black h-8 px-2.5 rounded-lg flex items-center justify-center hover:bg-neutral-800"
+            >
+              Criar evento
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
